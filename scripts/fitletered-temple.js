cff0ddefd4b1e1c6d9b4d3c692f7470ec639ada8
Doris Nnenna Amuji <dorisnnenna2016@gmail.com>
2026-06-06 15:27:58 +0100

    completed wk04

---
+++ scripts/fitletered-temple.js
@@ -0,0 +1,178 @@
+const year = document.querySelector("#currentyear");
+year.textContent = new Date().getFullYear();
+
+
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+// Hamburger Menu
+const menuButton = document.querySelector("#menu-button");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("open");
+
+    if (nav.classList.contains("open")) {
+        menuButton.textContent = "X";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
+
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+
+    {
+        templeName: "Accra Ghana",
+        location: "Accra, Ghana",
+        dedicated: "2004, January, 11",
+        area: 17500,
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/320x180/accra-ghana-temple-detail-249022-2400x1200.jpg"
+    },
+    {
+        templeName: "Kinshasa DRC",
+        location: "Kinshasa, Democratic Republic of the Congo",
+        dedicated: "2019, April, 14",
+        area: 12000,
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/kinshasa-democratic-republic-of-congo/320x200/01-Kinshasa-DRCongo-Temple-2208932.jpg"
+    },
+    {
+        templeName: "Montreal Quebec",
+        location: "Quebec, Canada",
+        dedicated: "2015, November, 22",
+        area: 19800,
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/montreal-quebec/400x250/montreal-quebec-temple-1169262-wallpaper.jpg"
+    }
+
+];
+
+function displayTemples(filteredTemples) {
+    const gallery = document.querySelector(".gallery");
+
+    gallery.innerHTML = "";
+
+    filteredTemples.forEach(temple => {
+
+        const card = document.createElement("section");
+
+        const name = document.createElement("h3");
+        const location = document.createElement("p");
+        const dedicated = document.createElement("p");
+        const area = document.createElement("p");
+        const image = document.createElement("img");
+
+        name.textContent = temple.templeName;
+        location.textContent = `Location: ${temple.location}`;
+        dedicated.textContent = `Dedicated: ${temple.dedicated}`;
+        area.textContent = `Area: ${temple.area.toLocaleString()} sq ft`;
+
+        image.src = temple.imageUrl;
+        image.alt = temple.templeName;
+        image.loading = "lazy";
+
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedicated);
+        card.appendChild(area);
+        card.appendChild(image);
+
+        gallery.appendChild(card);
+    });
+}
+
+displayTemples(temples);
+
+document.querySelector("#home").addEventListener("click", () => {
+    displayTemples(temples);
+});
+
+document.querySelector("#old").addEventListener("click", () => {
+
+    const oldTemples = temples.filter(
+        temple => new Date(temple.dedicated).getFullYear() < 1900
+    );
+
+    displayTemples(oldTemples);
+});
+
+document.querySelector("#new").addEventListener("click", () => {
+
+    const newTemples = temples.filter(
+        temple => new Date(temple.dedicated).getFullYear() > 2000
+    );
+
+    displayTemples(newTemples);
+});
+
+document.querySelector("#large").addEventListener("click", () => {
+
+    const largeTemples = temples.filter(
+        temple => temple.area > 90000
+    );
+
+    displayTemples(largeTemples);
+});
+
+document.querySelector("#small").addEventListener("click", () => {
+
+    const smallTemples = temples.filter(
+        temple => temple.area < 10000
+    );
+
+    displayTemples(smallTemples);
+});
